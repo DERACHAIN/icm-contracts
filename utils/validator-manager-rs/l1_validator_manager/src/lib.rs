bfdabe5493cd1fdc1eefce4cf43a449614def499
barchitect0 <tbng84@gmail.com>
2025-02-12 10:55:35 +0700

    validator manager lib

--- utils/validator-manager-rs/l1_validator_manager/src/lib.rs
+++ utils/validator-manager-rs/l1_validator_manager/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use ethers:: {
+    providers::{Http, Provider},
+    types::{Address, H256, Bytes},
+    contract::Contract,
+    core::abi::Abi,
+};
+use std::sync::Arc;
+use std::error::Error;
+
+pub struct ValidatorManager {
+    abi: Abi,
+    contract: Contract<Provider<Http>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl ValidatorManager {
+    pub fn new(rpc_url: &str, proxy_address: &str, abi_str: &str) -> Self {
+        let provider = Provider::<Http>::try_from(rpc_url).unwrap();
+        let client = Arc::new(provider);
+        let proxy_address: Address = proxy_address.parse().unwrap();
+        let abi: Abi = serde_json::from_str(abi_str).unwrap();
+        let contract: Contract<Provider<Http>> = Contract::new(proxy_address, abi.clone(), client.clone());
+
+        ValidatorManager {
+            abi,
+            contract,
+        }
+    }
+
+    pub async fn get_validation_id(&self, rpc_url: String, node_id: &str) -> Result<H256, Box<dyn Error>> {
+        let node_id_hex = hex::decode(node_id)?;
+        let validation_id: H256 = self.contract.method("registeredValidators", Bytes::from(node_id_hex))?.call().await?;
+        Ok(validation_id)
     }
 }
